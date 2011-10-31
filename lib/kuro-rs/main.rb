0d6dbf86fb99abc326bc80aeb10e6afc99c44840
Sho Hashimoto <hashimoto@shokai.org>
2011-10-31 17:09:03 +0900

    busy status

--- lib/kuro-rs/main.rb
+++ lib/kuro-rs/main.rb
@@ -9,10 +9,18 @@ module KuroRs
   class KuroRs
     attr_accessor :verbose
 
+    def busy? 
+      @busy
+    end
+
+    def serialport
+      @sp
+    end
+
     def initialize(device_name)
       @device_name = device_name
       @verbose = false
-
+      @busy = false
       if !File.exists? device_name.to_s
         device_name = "nil" if device_name.to_s.size < 1
         raise Error.new "device not found (#{device_name})"
@@ -25,59 +33,73 @@ module KuroRs
     end
     
     def read
-      ## request reading mode
-      @sp.write 'c'
-      @sp.write 'r'
-      puts 'read mode' if @verbose
-      raise Error.new 'response is not "Y"' unless @sp.getc.chr == 'Y'
+      @busy = true
       begin
-        timeout(5) do
+        ## request reading mode
+        @sp.write 'c'
+        @sp.write 'r'
+        puts 'read mode' if @verbose
+        raise Error.new 'response is not "Y"' unless @sp.getc.chr == 'Y'
+        timeout(30) do
           while @sp.getc.chr != 'S' do end
         end
-      rescue StandardError => e
-        raise Error.new(e)
+
+        ## read data
+        puts 'reading data..' if @verbose
+        data = (0...240).to_a.map{|i| @sp.getc} # read 240 Bytes
+        raise Error.new 'response is not "E"' unless @sp.getc.chr == 'E'
+      rescue Error, StandardError => e
+        @busy = false
+        raise Error.new e
       rescue Timeout::Error
+        @busy = false
         raise Error.new 'no response'
       end
       
-      ## read data
-      puts 'reading data..' if @verbose
-      data = Array.new
-      240.times do
-        data << @sp.getc
-      end
-      
-      raise Error.new 'response is not "E"' unless @sp.getc.chr == 'E'
-      return data.map{|i|
+      ## hex dump
+      hex_str = data.map{|i|
         h = i.to_s(16)
         h = "0#{h}" if h.size < 2
         h
       }.join('')
+      @busy = false
+      return hex_str
     end
     
     def write(hex_str)
+      @busy = true
       ## request writing mode
-      @sp.write 'c'
-      @sp.write 't'
-      raise Error.new 'response is not "Y"' unless @sp.getc.chr == 'Y'
-      @sp.write '1'
-      raise Error.new 'response is not "Y"' unless @sp.getc.chr == 'Y'
-      
+      begin
+        @sp.write 'c'
+        @sp.write 't'
+        raise Error.new 'response is not "Y"' unless @sp.getc.chr == 'Y'
+        @sp.write '1'
+        raise Error.new 'response is not "Y"' unless @sp.getc.chr == 'Y'
+      rescue Error, StandardError => e
+        @busy = false
+        raise Error.new e
+      end
+
       ## write hex string
       cmd_arr = []
-      arr = cmd_hex.split('')
+      arr = hex_str.split('')
       loop do
         s = arr.shift + arr.shift
         cmd_arr << s.hex
         break if arr.empty?
       end
       
-      puts "command size:#{cmd_arr.size}(bytes)" if @verbose
+      puts "command size:#{cmd_arr.size}(Bytes)" if @verbose
       puts 'writing data...' if @verbose
-      cmd_arr.each{|c|
-        @sp.putc c
-      }
-      raise Error.new 'response is not E' unless @sp.getc.chr == 'E'
+      begin
+        cmd_arr.each{|c|
+          @sp.putc c
+        }
+        raise Error.new 'response is not E' unless @sp.getc.chr == 'E'
+      rescue => e
+        @busy = false
+        raise Error.new e
+      end
     end
     
   end
